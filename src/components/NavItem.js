e6a991ddfe2c55cb17bfdf8bcb3070f70a6e4cd8
Yevhwnii <mirandoos3223@gmail.com>
2020-08-23 17:39:55 +0300

    fixed issue #1

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./NavItem.css";
 
 const NavItem = (props) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <li className="nav-item">
-      <a className="icon-button" href="/">
+      <a className="icon-button" onClick={() => setOpen(!open)} href="#">
         {props.icon}
       </a>
+      {open && props.children}
     </li>
   );
 };
