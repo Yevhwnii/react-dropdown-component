e0f4571e377a3be70c8653e0d24a341e86c29f60
Yevhwnii <mirandoos3223@gmail.com>
2020-08-23 17:40:23 +0300

    using dropdown

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import NavBar from "./components/NavBar";
 import NavItem from "./components/NavItem";
+import DropDownMenu from "./components/DropdownMenu";
 
 // Icons
 import { ReactComponent as PlusIcon } from "./icons/plus.svg";
 import { ReactComponent as BellIcon } from "./icons/bell.svg";
 import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CarretIcon } from "./icons/caret.svg";
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
       <NavItem icon={<PlusIcon />} />
       <NavItem icon={<BellIcon />} />
       <NavItem icon={<MessengerIcon />} />
+
+      <NavItem icon={<CarretIcon />}>
+        <DropDownMenu />
+      </NavItem>
     </NavBar>
   );
 }
