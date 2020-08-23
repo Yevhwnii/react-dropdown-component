ace2fab5cf82c10c562b7733c671b4583b1cd11e
Yevhwnii <mirandoos3223@gmail.com>
2020-08-23 16:16:21 +0300

    wiring up navbar and navitems

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import NavBar from "./components/NavBar";
+import NavItem from "./components/NavItem";
+
+// Icons
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+
 function App() {
-  return <div></div>;
+  return (
+    <NavBar>
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+    </NavBar>
+  );
 }
 
 export default App;
